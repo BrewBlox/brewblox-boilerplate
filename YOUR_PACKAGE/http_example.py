8949d6beb3145ff000736b6e12515df40c44baf9
Bob Steers <steers.bob@gmail.com>
2022-01-13 04:38:43 +0100

    use pydantic in http example

--- YOUR_PACKAGE/http_example.py
+++ YOUR_PACKAGE/http_example.py
@@ -3,7 +3,9 @@ An example of how to register HTTP endpoints
 """
 
 from aiohttp import web
-from aiohttp_apispec import docs
+from aiohttp_pydantic import PydanticView
+from aiohttp_pydantic.oas.typing import r200
+from pydantic import BaseModel
 
 routes = web.RouteTableDef()
 
@@ -13,36 +15,46 @@ def setup(app: web.Application):
     app.router.add_routes(routes)
 
 
-@docs(
-    tags=['Example'],
-    summary='Example endpoint',
-    description='An example of how to use aiohttp features',
-    parameters=[{
-        'in': 'body',
-        'name': 'body',
-        'description': 'Input message',
-        'required': 'true',
-        'schema': {
-            'type': 'object',
-            'required': ['message'],
-            'properties': {
-                'message': {'type': 'string'}
-            },
-        },
-    }],
-)
-@routes.post('/example/endpoint')
-async def example_endpoint_handler(request: web.Request) -> web.Response:
+class EndpointMessage(BaseModel):
     """
-    Example endpoint handler. Using `routes.post` means it will only respond to POST requests.
-
-    Each aiohttp endpoint should take a request as argument, and return a response.
-    You can use aiohttp-apispec to document your endpoint.
-    This adds the endpoint to those shown in the /{name}/api/doc page.
-
-    You can also use marshmallow to validate input.
-    For more information, see: https://aiohttp-apispec.readthedocs.io/en/latest/index.html
+    For more options, see https://pydantic-docs.helpmanual.io/
     """
-    args = await request.json()
-    message = args['message']
-    return web.Response(body=f'Hello world! (You said: "{message}")')
+    content: str
+
+
+@routes.view('/example/endpoint')
+class ExampleEndpoint(PydanticView):
+    """
+    When using the PydanticView, the endpoint is automatically added to the /api/doc page.
+    For more information, see: <br>
+    - https://docs.aiohttp.org/en/stable/web_quickstart.html#class-based-views <br>
+    - https://github.com/Maillol/aiohttp-pydantic <br>
+    """
+
+    async def get(self) -> r200[EndpointMessage]:
+        """
+        Example endpoint handler for GET requests.
+        The docstring message will be automatically added to the /api/doc page.
+
+        The `r200[EndpointMessage]` indicates that if everything goes ok (status 200),
+        the response body contains an EndpointMessage.
+
+        Tags: Example, Endpoint
+        """
+        return web.json_response(
+            EndpointMessage(content='Hello world!').dict()
+        )
+
+    async def post(self, message: EndpointMessage) -> r200[EndpointMessage]:
+        """
+        Example endpoint handler for POST requests.
+        The docstring message will be automatically added to the /api/doc page.
+
+        When using PydanticView, you can add arguments to post / put / patch functions.
+        The JSON in the request body will be automatically parsed and validated.
+
+        Tags: Example, Endpoint
+        """
+        return web.json_response(
+            EndpointMessage(content=f'Hi! You said `{message.content}`.').dict()
+        )
